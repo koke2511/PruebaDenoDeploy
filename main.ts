ea327241135d3ed1344df23636c9c9b0f8b14c8f
koke2511 <j.d.ferrero@hotmail.com>
2025-01-30 23:01:05 +0100

    Nuevo commint

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { ApolloServer } from "@apollo/server";
+import { MongoClient, Collection } from "mongodb";
+import { ContactoModel } from "./types.ts";
+import {startStandaloneServer } from "@apollo/server/standalone";
+import { resolvers } from "./resolvers.ts";
+import { schema } from "./schema.ts";
+
+const MONGO_URL = Deno.env.get("MONGO_URL");
+
+if(!MONGO_URL) {
+  throw new Error("Please provide a MONGO_URL");
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const mongoClient = new MongoClient(MONGO_URL);
+await mongoClient.connect();
+
+console.info("Connected to MongoDB");
+
+const mongoDB = mongoClient.db("agenda");
+const ContactoCollection = mongoDB.collection<ContactoModel> ("contactos");
+
+const server = new ApolloServer({
+  typeDefs: schema, 
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  context: async() => ({ContactoCollection}),
+});
+
+console.info(`Server ready at ${url}`);
